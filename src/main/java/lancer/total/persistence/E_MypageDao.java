574b064815168579497c5fe36878e0a87ecfd387
kosta <kosta@kosta-PC>
2017-06-01 15:53:58 +0900

    커밋

--- src/main/java/lancer/total/persistence/E_MypageDao.java
+++ src/main/java/lancer/total/persistence/E_MypageDao.java
@@ -1,14 +1,14 @@
 package lancer.total.persistence;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import javax.inject.Inject;
 
-import org.apache.ibatis.session.RowBounds;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
+
+import lancer.e_mypage.domain.Enterprise;
+
+
 
 @Repository
 public class E_MypageDao{
@@ -16,9 +16,77 @@ public class E_MypageDao{
 	@Inject
 	private SqlSession session;
 	
-	private static String namespace = "lancer.total.mappers.E_MypageMapper";
+	private static String namespace = "lancer.mappers.e_mypageMapper";
 	
+	public Enterprise selectEnterprise(int e_num) throws Exception {
+		return session.selectOne(namespace+".selectEnterprise", e_num);
+	}
 	
+	public void updateEnterprise(Enterprise enterprise) throws Exception {
+		session.update(namespace+".updateEnterprise", enterprise);
+	}
+	
+	/*
+
+	
+	public List<Project> listProjectReady(int e_num){
+		SqlSession session = getSqlSessionFactory().openSession();
+		try {
+			return session.getMapper(E_MypageMapper.class).listProjectReady(e_num);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		} finally {
+			session.close();
+		}
+	}
+	public List<Project> listProjectDoing(int e_num){
+		SqlSession session = getSqlSessionFactory().openSession();
+		try {
+			return session.getMapper(E_MypageMapper.class).listProjectDoing(e_num);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		} finally {
+			session.close();
+		}
+	}
+	public List<Project> listProjectDone(int e_num){
+		SqlSession session = getSqlSessionFactory().openSession();
+		try {
+			return session.getMapper(E_MypageMapper.class).listProjectDone(e_num);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		} finally {
+			session.close();
+		}
+	}
+	
+	public Project selectProject(HashMap<String, Integer> map){
+		SqlSession session = getSqlSessionFactory().openSession();
+		try {
+			return session.getMapper(E_MypageMapper.class).selectProject(map);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		} finally {
+			session.close();
+		}
+	}
+	
+	public List<Integer> selectP_job(int e_pr_num){
+		SqlSession session = getSqlSessionFactory().openSession();
+		try {
+			return session.getMapper(E_MypageMapper.class).selectP_job(e_pr_num);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		} finally {
+			session.close();
+		}
+	}
+	*/
 	
 	/*@Override
 	public void create(BoardVO vo) throws Exception {
